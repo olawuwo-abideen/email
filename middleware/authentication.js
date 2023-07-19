a4f26fb2f657b5ea94103eee0e232f6bfe2a7d90
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-07-19 07:12:26 +0100

    fix

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const { UnauthenticatedError } = require('../errors')
 
 const auth = async (req, res, next) => {
-  // check header
+
   const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnauthenticatedError('Authentication invalid')
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
-    // attach the user to the job routes
+    
     req.user = { userId: payload.userId, name: payload.name }
     next()
   } catch (error) {
