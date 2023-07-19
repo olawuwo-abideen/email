22c33b443f9a74b5bb61fadd0843a37a99f38fb2
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-07-19 07:24:49 +0100

    push

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 require('dotenv').config();
-
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
 
+const sendEmail = require('./controllers/email');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Email Project</h1> <a href="/send">send email</a>');
+});
+
+app.get('/send', sendEmail);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 
